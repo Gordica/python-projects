baa0b0b8caa6250e5d4ef5a15deb7d8ef5ac95aa
Gordica <Gordica2@Gmail.com>
2014-07-24 12:32:40 -0800

    my first if, elif, and else

---
+++ hello all.py
@@ -0,0 +1,12 @@
+
+
+answer = input("what is your name?")
+
+if answer == "sven":
+    print("o hay..." + answer)
+
+elif answer == "meepo":
+   print("meepo,MEEPO,MEEEEEEPPOOOO!")
+
+else:
+   print("hi " + answer)
