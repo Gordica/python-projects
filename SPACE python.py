db4352982e67076785f890a6dc980c6d100f5e12
Gordica <Gordica2@Gmail.com>
2014-07-28 12:35:06 -0800

    many

---
+++ SPACE python.py
@@ -0,0 +1,20 @@
+
+print("hello world, and hi... person")
+name = input("what is your name")
+
+answer  = input("hi" + name + ", how are you doing?")
+
+if answer == "good":
+    print("thats good!")
+
+elif answer == "bad":
+    print("awwww.")
+
+elif answer == ("IM IN SPACE!")
+    print("amazing!")
+
+elif answer == ("im dead")
+    print("thats bad... you should get that checked out")
+
+else:
+    print("ok")
